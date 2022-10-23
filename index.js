a2429e0657c634d3bf4d9d26549f13cddb0e3207
Ndhossain <hhhssnahiid@gmail.com>
2022-10-23 15:28:39 +0600

    server work done

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-
 app.use(cors());
 
 const categories = require('./data/categories.json');
